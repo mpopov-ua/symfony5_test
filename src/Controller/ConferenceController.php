41258592c1e75137c1dee3f47032644ab9cf5d8f
Maksym Popov <maksym.popov@branderstudio.com>
2021-11-04 11:29:55 +0200

    Add HTML form

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Conference;
+use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -48,6 +50,9 @@ class ConferenceController extends AbstractController
     #[Route ('/conference/{slug}', name: 'conference_slug')]
     public function show (Request $request, Conference $conference, CommentRepository $commentRepository, ConferenceRepository$conferenceRepository)
     {
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class, $comment);
+
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
 
@@ -61,7 +66,8 @@ class ConferenceController extends AbstractController
 //               ])
             'comments'=>$paginator,
             'previous'=>$offset- CommentRepository::PAGINATOR_PER_PAGE,
-            'next'=>min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE)
+            'next'=>min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            'comment_form'=>$form->createView()
 
             ]));
     }
