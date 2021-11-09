ce9e22523a411fe1349cfdde5be0b87ff1e64e60
Maksym Popov <maksym.popov@branderstudio.com>
2021-11-09 18:30:34 +0200

    Add SenderMail and config that send local with strange SMTP

--- src/MessageHandler/CommentMessageHandler.php
+++ src/MessageHandler/CommentMessageHandler.php
@@ -5,21 +5,36 @@ namespace App\MessageHandler;
 use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
 use App\SpamChecker;
+use Doctrine\ORM\Cache\Exception\NonCacheableEntity;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Bridge\Twig\Mime\NotificationEmail;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Workflow\WorkflowInterface;
 
 class CommentMessageHandler implements MessageHandlerInterface
 {
     private $spamChecker;
     private $entityManager;
     private $commentRepository;
+    private $bus;
+    private $workflow;
+    private $mailer;
+    private $adminEmail;
+    private $logger;
 
-    public function __construct (EntityManagerInterface $entityManager, SpamChecker $spamChecker, CommentRepository $commentRepository)
+    public function __construct (EntityManagerInterface $entityManager, SpamChecker $spamChecker, CommentRepository $commentRepository, MessageBusInterface $bus, WorkflowInterface $commentStateMachine, MailerInterface $mailer, string $adminEmail, LoggerInterface $logger = null)
     {
         $this->entityManager=$entityManager;
         $this->spamChecker=$spamChecker;
         $this->commentRepository=$commentRepository;
-
+        $this->bus=$bus;
+        $this->workflow=$commentStateMachine;
+        $this->mailer=$mailer;
+        $this->adminEmail=$adminEmail;
+        $this->logger=$logger;
     }
 
     public function __invoke(CommentMessage $message)
@@ -28,11 +43,36 @@ class CommentMessageHandler implements MessageHandlerInterface
         if (!$comment) {
             return;
         }
-        if (2 === $this->spamChecker->getSpamScope ($comment, $message->getContext())) {
-            $comment->setState('spam');
-        } else {
-            $comment->setState('published');
+//        }
+//        if (2 === $this->spamChecker->getSpamScope ($comment, $message->getContext())) {
+//            $comment->setState('spam');
+//        } else {
+//            $comment->setState('published');
+//        }
+        if ($this->workflow->can($comment, 'accept')) {
+            $scope = $this->spamChecker->getSpamScope($comment, $message->getContext());
+            $transition = 'accept';
+            if (2 === $scope) {
+                $transition = 'reject_spam';
+            } elseif (1 === $scope) {
+                $transition = 'might_be_spam';
+            }
+            $this->workflow->apply($comment, $transition);
+            $this->entityManager->flush();
+
+            $this->bus->dispatch($message);
+        } elseif ($this->workflow->can($comment, 'publish') || $this->workflow->can($comment, 'publish_ham')) {
+//            $this->workflow->apply($comment, $this->workflow->can($comment, 'publish') ? 'publish' : 'publish_ham');
+//            $this->entityManager->flush();
+                $this->mailer->send((new NotificationEmail())
+                ->subject('New comment posted')
+                ->htmlTemplate('emails/comment_notification.html.twig')
+                ->from($this->adminEmail)
+                ->to($this->adminEmail)
+                 ->context(['comment'=>$comment])
+                );
+            } elseif ($this->logger) {
+                $this->logger->debug('Dropping comment message', ['comment'=>$comment->getId(), 'state'=>$comment->getState()]);
         }
-        $this->entityManager->flush();
     }
 }
