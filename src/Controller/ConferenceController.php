e6c78e61392e0afc803a1cd91a7e0d017a6e2453
Maksym Popov <maksym.popov@branderstudio.com>
2021-11-10 19:03:14 +0200

    Add Cache and Command

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -52,9 +52,22 @@ class ConferenceController extends AbstractController
 //</html>
 //EOF
 //);
-        return new Response($this->twig->render('conference/index.html.twig', [
+        $response = new Response($this->twig->render('conference/index.html.twig', [
             'conferences'=>$conferenceRepository->findAll(),
         ]));
+        $response->setSharedMaxAge(3600);
+
+        return $response;
+    }
+    #[Route('/conference_header', name: 'conference_header')]
+    public function conferenceHeader(ConferenceRepository $conferenceRepository, ): Response
+    {
+        $response = new Response($this->twig->render('conference/header.html.twig', [
+            'conferences'=>$conferenceRepository->findAll()
+        ]));
+        $response->setSharedMaxAge(3600);
+
+        return $response;
     }
     #[Route ('/conference/{slug}', name: 'conference_slug')]
     public function show (Request $request, Conference $conference, CommentRepository $commentRepository, ConferenceRepository$conferenceRepository, string $photoDir)
