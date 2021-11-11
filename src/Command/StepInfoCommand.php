86f6d39f4317a08bc611402d6be11ce445efd139
Maksym Popov <maksym.popov@branderstudio.com>
2021-11-11 18:38:08 +0200

    Add Webpack, SCSS, Nodejs and much more interesting thing for Frontend

--- src/Command/StepInfoCommand.php
+++ src/Command/StepInfoCommand.php
@@ -9,6 +9,8 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Process\Process;
+use Symfony\Contracts\Cache\CacheInterface;
 
 #[AsCommand(
     name: 'app:step:info',
@@ -16,6 +18,14 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 )]
 class StepInfoCommand extends Command
 {
+    protected static $defaultName = 'app:step:info';
+    protected $cache;
+    public function __construct(CacheInterface $cache)
+    {
+        $this->cache=$cache;
+        parent::__construct();
+    }
+
     protected function configure(): void
     {
         $this
@@ -26,18 +36,26 @@ class StepInfoCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
+        $step = $this->cache->get('app.current_step', function ($item) {
+            $process = new Process(['git', 'tag', '-l', '--points-at', 'HEAD']);
+            $process->mustRun();
+            $item->expiresAfter(30);
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
-        }
-
-        if ($input->getOption('option1')) {
-            // ...
-        }
-
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+            return $process->getOutput();
+        });
+        $output->writeln($step);
+//        $io = new SymfonyStyle($input, $output);
+//        $arg1 = $input->getArgument('arg1');
+//
+//        if ($arg1) {
+//            $io->note(sprintf('You passed an argument: %s', $arg1));
+//        }
+//
+//        if ($input->getOption('option1')) {
+//            // ...
+//        }
+//
+//        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
 
         return Command::SUCCESS;
     }
--- src/Repository/ConferenceRepository.php
+++ src/Repository/ConferenceRepository.php
@@ -9,7 +9,6 @@ use Doctrine\Persistence\ManagerRegistry;
 /**
  * @method Conference|null find($id, $lockMode = null, $lockVersion = null)
  * @method Conference|null findOneBy(array $criteria, array $orderBy = null)
- * @method Conference[]    findAll()
  * @method Conference[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class ConferenceRepository extends ServiceEntityRepository
@@ -26,7 +25,7 @@ class ConferenceRepository extends ServiceEntityRepository
     {
         return $this->findBy([],['year'=>'ASC', 'city'=>'ASC']);
     }
-
+     //* @method Conference[]    findAll()
     // /**
     //  * @return Conference[] Returns an array of Conference objects
     //  */
