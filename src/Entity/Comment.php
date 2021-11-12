96f1f17bf545a03654efdced88ca4dd9e4980355
Maksym Popov <maksym.popov@branderstudio.com>
2021-11-12 18:02:18 +0200

    Add Api platform and wrote annotation

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -2,14 +2,25 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\CommentRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Form\Extension\Core\DataTransformer\DateTimeImmutableToDateTimeTransformer;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=CommentRepository::class)
  * @ORM\HasLifecycleCallbacks()
+ *
+ * @ApiResource(
+ *     collectionOperations={"get"={"normalization_context"={"groups"="comment:list"}}},
+ *     itemOperations={"get"={"normalization_context"={"groups"="comment:item"}}},
+ *     order={"createdAt"="DESC"},
+ *     paginationEnabled=false
+ * )
+ * @ApiFilter(SearchFilter::class, properties={"conference": "exact"})
  *
  */
 class Comment
@@ -19,18 +30,21 @@ class Comment
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
+    #[Groups(['comment:list', 'comment:item'])]
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
     #[Assert\NotBlank]
+    #[Groups(['comment:list', 'comment:item'])]
     private $author;
 
     /**
      * @ORM\Column(type="text")
      */
     #[Assert\NotBlank]
+    #[Groups(['comment:list', 'comment:item'])]
     private $text;
 
     /**
@@ -38,21 +52,25 @@ class Comment
      */
     #[Assert\NotBlank]
     #[Assert\Email]
+    #[Groups(['comment:list', 'comment:item'])]
     private $email;
 
     /**
      * @ORM\Column(type="datetime_immutable")
      */
+    #[Groups(['comment:list', 'comment:item'])]
     private $createdAt;
 
     /**
      * @ORM\ManyToOne(targetEntity=Conference::class, inversedBy="comments")
      */
+    #[Groups(['comment:list', 'comment:item'])]
     private $conference;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
      */
+    #[Groups(['comment:list', 'comment:item'])]
     private $photoFileName;
 
     /**
--- src/Entity/Conference.php
+++ src/Entity/Conference.php
@@ -2,17 +2,26 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\ConferenceRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use JetBrains\PhpStorm\Pure;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
  * @ORM\Entity(repositoryClass=ConferenceRepository::class)
  * @UniqueEntity("slug")
+ *
+ * @ApiResource(
+ *     collectionOperations={"get"={"normalization_context"={"groups"="conference:list"}}},
+ *     itemOperations={"get"={"normalization_context"={"groups"="conference:item"}}},
+ *     order={"year"="DESC", "city"="ASC"},
+ *     paginationEnabled=false
+ * )
  */
 class Conference
 {
@@ -21,31 +30,37 @@ class Conference
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
+    #[Groups(['conference:list', 'conference:item'])]
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
+    #[Groups(['conference:list', 'conference:item'])]
     private $city;
 
     /**
      * @ORM\Column(type="string", length=4)
      */
+    #[Groups(['conference:list', 'conference:item'])]
     private $year;
 
     /**
      * @ORM\Column(type="boolean")
      */
+    #[Groups(['conference:list', 'conference:item'])]
     private $isInternational;
 
     /**
      * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="conference")
      */
+    #[Groups(['conference:list', 'conference:item'])]
     private $comments;
 
     /**
      * @ORM\Column(type="string", length=255, unique=true)
      */
+    #[Groups(['conference:list', 'conference:item'])]
     private $slug;
 
     #[Pure] public function __construct()
@@ -105,7 +120,7 @@ class Conference
     }
 
     /**
-     * @return Collection|Comment[]
+     * @return Collection
      */
     public function getComments(): Collection
     {
